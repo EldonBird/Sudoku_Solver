85181f02521f5198cccb99f3a60352dd42de0b6d
Eldon <Birdheldon@gmail.com>
2025-02-02 23:06:06 -0700

    fff

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,8 +1,13 @@
+use std::io::empty;
 
-struct tile{
-    found: i8,
-    possible: [i8; 0],
+struct Tile{
+    found: i8, // for testing, but the found solution
+    possible: [i8; 0], // all possible solutions
+    position: (i8, i8), // x, y
+}
 
+struct Square{
+    tiles: [Tile; 9],
 }
 
 
@@ -12,14 +17,42 @@ fn main() {
 
 
 
+fn EvaluateBoard() -> [[i8; 9]; 9] {
+
+
+    return null;
+}
+
 fn EvaluateTile() -> [i8; 9] {
+
+
+
+
+    return null;
+}
+
+fn EvaluateByRow(tile: Tile){
+
 
 
 
 }
 
-fn EvaluateBoard() ->  {
+fn EvaluateByColumn(tile: Tile){
 
+
+
+}
+
+fn EvaluateSquare(){ // returns if a valid square
+
+
+
+
+}
+
+
+fn AvailableByRow(){ // returns remaining possible numbers in a row
 
 
 
