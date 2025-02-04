13b09afb0e920e89bb4f07feb477b722fed11000
Eldon <Birdheldon@gmail.com>
2025-02-04 01:13:15 -0700

    added Row&column Evaluations

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,9 +1,13 @@
+use std::any::Any;
 use std::io::empty;
 
 struct Tile{
-    found: i8, // for testing, but the found solution
-    possible: [i8; 0], // all possible solutions
+    found: bool,
+    final_guess: i8, // for testing, but the found solution
+    possible: [bool; 9], // all possible solutions, true if available, false if not 0 = 1, so 8 = 9
     position: (i8, i8), // x, y
+
+
 }
 
 struct Square{
@@ -17,42 +21,62 @@ fn main() {
 
 
 
-fn EvaluateBoard() -> [[i8; 9]; 9] {
+fn EvaluateBoard() -> [[Tile; 9]; 9] {
 
 
     return null;
 }
 
-fn EvaluateTile() -> [i8; 9] {
+fn EvaluateTile(tile: Tile, board: [[Tile; 9]; 9]) -> [bool; 9] {
 
-
+    let Xpos = tile.position.0;
 
 
     return null;
 }
 
-fn EvaluateByRow(tile: Tile){
+
+// returns an array of booleans, the index is the possible number(-1) and true means possible valid, false means invalid
+fn EvaluateByRow(tile: Tile, board: [[Tile; 9]; 9]) -> [bool; 9]{
+
+    let row = board[tile.position.1]; // 0=x 1=y
+
+    let mut checking_array = [true; 9];
 
 
+    // ideally this will always work, although there isnt any guess work to find incorect options
+    for element in row.iter(){
+        if(element.found){
+            checking_array[element.final_guess] = false;
+        }
+    }
 
 
+    return checking_array;
 }
 
-fn EvaluateByColumn(tile: Tile){
+fn EvaluateByColumn(tile: Tile, board: [[Tile; 9]; 9]) -> [bool; 9]{
+
+    let mut column: [Tile; 9] = [Tile::defualt(); 9];
+    let xpos = tile.position.0; // 0=x 1=y
+
+    for i in 0..8{
+        column[i] = board[i][xpos];
+    }
+
+    let mut checking_array = [true; 9];
+
+    for element in column.iter(){
+        if(element.found){
+            checking_array[element.final_guess] = false;
+        }
+    }
 
 
-
+    return checking_array;
 }
 
 fn EvaluateSquare(){ // returns if a valid square
-
-
-
-
-}
-
-
-fn AvailableByRow(){ // returns remaining possible numbers in a row
 
 
 
